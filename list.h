e161907aba17fdb3400f03f9984e95ddd74d993c
braindeadboooooy <58190010+braindeadboooooy@users.noreply.github.com>
2020-06-30 14:18:10 +0300

    Update list.h

--- list.h
+++ list.h
@@ -15,7 +15,6 @@ public:
     void add(string d_spec, string d_lname, string d_fname, string p_lname, string p_fname, int day, int month, int hour, int minutes);
     void remove(int n);
     void printAll();
-    void removeAll();
     void search(string usearch);
 
 private:
